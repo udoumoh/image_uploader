5eb4ff69b9c07c3e8b293f048a2fcc691f0278e4
John <johnudoumoh63@gmail.com>
2023-07-03 17:29:50 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Grid,
-  Card,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+import Upload from './upload';
 // import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-            <Card>
-
-            </Card>
-        </Grid>
-      </Box>
+      <Upload />
     </ChakraProvider>
   );
 }
---
+++ src/upload.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+    Box,
+    Grid,
+    Card,
+    Text,
+    GridItem,
+    Center,
+} from '@chakra-ui/react';
+
+const Upload = () => {
+  return (
+    <Center>
+      <Box textAlign={'center'} fontSize={'30'} w={'100%'} border={'rounded'}>
+          <Grid templateColumns='repeat(1, 1fr)' placeItems={'center'}>
+              <GridItem w='40%' h='20'>
+                  <Card boxShadow='lg' alignItems='center'>
+                      <Text fontWeight={'500'} color={'gray.600'}>Upload your Image</Text>
+                      <Text fontSize={'15'} mt={'19'} color={'GrayText'} fontWeight={'500'}>File Should be Jpeg, Png...</Text>
+
+                      <Box border={'dashed'} borderColor={'#97BEF4'} borderWidth={'2px'} rounded={'xl'} h={'300'} w={'70%'} my='19' backgroundColor={'gray.50'}>
+
+                      </Box>
+                  </Card>
+              </GridItem>
+          </Grid>
+      </Box>
+    </Center>
+  )
+}
+
+export default Upload
